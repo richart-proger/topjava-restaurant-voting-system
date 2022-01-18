c7e3f48fe2c052765a433a6a9e4309556a4876c4
Artur Galimov <satorica@mail.ru>
2022-01-18 19:55:28 +0300

    add hsqldb, init/populate db

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Restaurant voting system #
 >
-> Software requirements in <a href="description.md">REQUIREMENTS.md</a> file.
+> Software requirements in <a href="REQUIREMENTS.md">REQUIREMENTS.md</a> file.
 
 ### Technology stack:
 * Java 17
---
+++ src/main/resources/db/populateDB.sql
@@ -0,0 +1,40 @@
+DELETE
+FROM user_roles;
+DELETE
+FROM dishes;
+DELETE
+FROM restaurants;
+DELETE
+FROM users;
+DELETE
+FROM votes;
+ALTER SEQUENCE global_seq RESTART WITH 100000;
+
+INSERT INTO users (name, email, password)
+VALUES ('User', 'user@yandex.ru', 'password'),
+       ('Admin', 'admin@gmail.com', 'admin');
+
+INSERT INTO user_roles (role, user_id)
+VALUES ('USER', 100000),
+       ('ADMIN', 100001);
+
+INSERT INTO restaurants (name)
+VALUES ('Italian restaurant'),
+       ('Japan restaurant'),
+       ('French restaurant');
+
+INSERT INTO dishes (restaurant_id, name, price)
+VALUES (100002, 'Pizza', 600),
+       (100002, 'Fettuccine', 700),
+       (100003, 'Sushi Set', 1350),
+       (100003, 'Sashimi Set', 1100),
+       (100004, 'Ratatouille', 700),
+       (100004, 'Onion soup', 500);
+
+INSERT INTO dishes (restaurant_id, name, price, date)
+VALUES (100002, 'Pizza', 600, DATE_ADD(CURRENT_DATE, INTERVAL 2 DAY)),
+       (100002, 'Fettuccine', 700, DATE_ADD(CURRENT_DATE, INTERVAL 2 DAY));
+
+INSERT INTO votes (user_id, restaurant_id, booking_date)
+VALUES (100000, 100002, DATE_ADD(CURRENT_DATE, INTERVAL 2 DAY)),
+       (100001, 100004, DATE_ADD(CURRENT_DATE, INTERVAL 2 DAY));
