06ffc254e41d950d3709037c051530ae35f6d7a3
Artur Galimov <satorica@mail.ru>
2022-01-20 22:19:29 +0300

    test UserService

--- src/main/java/ru/javawebinar/restaurant_voting_system/service/UserService.java
+++ src/main/java/ru/javawebinar/restaurant_voting_system/service/UserService.java
@@ -1,5 +1,6 @@
 package ru.javawebinar.restaurant_voting_system.service;
 
+import org.springframework.stereotype.Service;
 import ru.javawebinar.restaurant_voting_system.model.User;
 import ru.javawebinar.restaurant_voting_system.repository.UserRepository;
 
@@ -8,9 +9,14 @@ import java.util.List;
 import static ru.javawebinar.restaurant_voting_system.util.ValidationUtil.checkNotFound;
 import static ru.javawebinar.restaurant_voting_system.util.ValidationUtil.checkNotFoundWithId;
 
+@Service
 public class UserService {
 
-    private UserRepository repository;
+    private final UserRepository repository;
+
+    public UserService(UserRepository repository) {
+        this.repository = repository;
+    }
 
     public User create(User user) {
         return repository.save(user);
